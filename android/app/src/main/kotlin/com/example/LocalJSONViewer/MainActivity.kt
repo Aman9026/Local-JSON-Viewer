0e7afb80279fdbccabefe39e280dcacccedd485d
Aman9026 <amangupta884036@gmail.com>
2020-09-17 00:57:42 +0530

    basic structure initialized

---
+++ android/app/src/main/kotlin/com/example/LocalJSONViewer/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.LocalJSONViewer
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(new MaterialApp(
+      theme: new ThemeData(
+        primarySwatch: Colors.teal,
+      ),
+      home: new HomePage(),
+    ));
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
